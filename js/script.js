369491bbe0454ff293047cd140bf4df78e664efb
Bartosz <bartoszbobrecki1@gmail.com>
2022-04-24 15:42:38 +0200

    fixed burger icon, destroyed dark section and white section

--- js/script.js
+++ js/script.js
@@ -1,1 +1,14 @@
-// to do in the future
+const burgerBtn = document.querySelector('.nav-items__burger-menu__burger-btn')
+const burgerBtnBurger = document.querySelector('.nav-items__burger-menu__burger-btn__burger-icon')
+const burgerBtnClose = document.querySelector('.nav-items__burger-menu__burger-btn__close-icon')
+const burgerMenu = document.querySelector('.nav-items__burger-menu__burger-window')
+
+
+const openMenu = () => {
+    burgerBtnBurger.classList.toggle('disabled')
+    burgerBtnClose.classList.toggle('blockActive')
+    burgerMenu.classList.toggle('flexActive')
+}
+burgerBtn.addEventListener('click', openMenu)
+console.log(burgerBtnBurger);
+console.log(burgerBtn);
